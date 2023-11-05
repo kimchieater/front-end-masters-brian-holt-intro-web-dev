17ef00c6a63638a8c7d53e33cdf48aecbc7a6140
kimchieater <jasonj19276@gmail.com>
2023-11-05 23:48:56 +1100

    finished with markings for words

--- wordle/script.js
+++ wordle/script.js
@@ -4,6 +4,14 @@ const ANSWER_LENGTH = 5;
 
 async function init() {
   let currentGuess = "";
+  let currentRow = 0;
+
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const resobj = await res.json();
+  const word = resobj.word.toUpperCase();
+  const wordParts = word.split("");
+  console.log(word);
+  setLoading(false);
 
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
@@ -12,12 +20,51 @@ async function init() {
       currentGuess =
         currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
-    inputText[currentGuess.length - 1].innerText = letter;
+    inputText[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText =
+      letter;
+  }
+
+  async function commit() {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      return;
+    }
+    //TODO validate the word
+
+    //TODO do all the marking
+    const map = makeMap(wordParts);
+    const guessParts = currentGuess.split("");
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      //mark as correct
+      if (guessParts[i] === wordParts[i]) {
+        inputText[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+        map[guessParts[i]]--;
+      }
+    }
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        //do nothing
+      } else if (wordParts.includes(guessParts[i]) && map[guessparts[i]] > 0) {
+        inputText[currentRow * ANSWER_LENGTH + i].classList.add("close");
+        map[guessParts[i]]--;
+      } else {
+        inputText[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
+    //TODO did they win or lose?
+
+    currentRow++;
+    currentGuess = "";
+  }
+
+  function backSpace() {
+    currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+    inputText[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
   }
 
   document.addEventListener("keydown", function handleKeypress(event) {
     const action = event.key;
-    console.log(action);
 
     if (action === "Enter") {
       commit();
@@ -33,6 +80,23 @@ async function init() {
 
 init();
 
+function setLoading(isLoading) {
+  loadingDiv.classList.toggle("hidden", !isLoading);
+}
+
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
+}
+
+function makeMap(array) {
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+    return obj;
+  }
 }
